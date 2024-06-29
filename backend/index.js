39eb4532af3444efeb7e3123bb3c67ed0a5a8cd0
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2024-06-29 15:28:39 +0500

    url changes

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,12 @@ import route from './routes/userRoute.js';
 const app =express();
 
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
