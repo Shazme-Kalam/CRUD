4029a30e256b1c9c2d74090b2ba053f0573025a5
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2024-06-29 15:46:31 +0500

    updated url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app =express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://crud-one-eta.vercel.app/"],
+    origin: ["crud-mern-app-six.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
