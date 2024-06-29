6799e2a0d58175ffaa2a5585687f77aaf1e615b2
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2024-06-29 16:06:18 +0500

    blank

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app =express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["crud-mern-app-six.vercel.app"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true
 }));
