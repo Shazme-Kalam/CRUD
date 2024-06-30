74228f4e0d4825953a42a851274c83c934b4ab34
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2024-06-30 10:11:59 +0500

    new

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import route from './routes/userRoute.js';
 
-dotenv.config(); // Load environment variables first
+dotenv.config(); 
 
 const app = express();
 
